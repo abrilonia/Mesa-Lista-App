bd7effd8b4af0067f51504208c75eeec797adf91
LucasA75 <lucasacevedocastro@gmail.com>
2023-03-24 16:52:53 -0300

    Javascript Puro - Formulario - Estilos Tabla

---
+++ main.js
@@ -0,0 +1,91 @@
+const form = document.forms["form"];
+const TBvalores = document.getElementById("ventaDeProductos").getElementsByTagName("tbody")[0];
+const buttonSubmit = document.querySelector(".submit-btn")
+const totalValores = document.getElementById("valorTotal")
+
+
+
+/* Funcionalidad botton submit */
+buttonSubmit.onclick = function (e) {
+    e.preventDefault()
+    /* Hacemos que no se recarge la pagina al enviar el formulario */
+
+    const producto = form.elements['producto'].value
+    const cantidad = form.elements['cantidad'].value
+    const precio = obtenerPrecio(producto);
+    const subTotal = precio * cantidad
+
+    if(producto === ""){
+        alert("Escoje un producto")
+        window.location.reload() /*  No es lo mejor pero es lo que se me ocurre :( */
+    }
+
+
+    /* Buscaremos una fila tiene ya el producto */
+    let filaExistente = null;
+
+    for (i = 0; i < TBvalores.rows.length; i++) {
+        const celdaProducto = TBvalores.rows[i].cells[0]; /* Aqui la misma celdas en distintas filas */
+        if (celdaProducto.innerHTML === producto) {
+            filaExistente = TBvalores.rows[i];
+            break;
+        }
+    }
+
+    if (filaExistente) {
+        //si existe una fila, sobresbribir los datos de la fila
+        const celdaCantidad = filaExistente.cells[1];
+        celdaCantidad.innerHTML = cantidad
+
+        const celdaPrecio = filaExistente.cells[2];
+        celdaPrecio.innerHTML = precio
+
+        const celdaSubTotal = filaExistente.cells[3];
+        celdaSubTotal.innerHTML = subTotal
+        
+    }
+    else {
+        //si no existe la fila, crear una nueva
+        const fila = TBvalores.insertRow();
+
+        const celdaProducto = fila.insertCell();
+        celdaProducto.innerHTML = producto
+
+        const celdaCantidad = fila.insertCell();
+        celdaCantidad.innerHTML = cantidad
+
+        const celdaPrecio = fila.insertCell();
+        celdaPrecio.innerHTML = precio
+
+        const celdaSubTotal = fila.insertCell();
+        celdaSubTotal.innerHTML = subTotal
+    }
+
+    actualizarTotal()
+}
+
+function obtenerPrecio(producto) {
+    // Esta función obtiene el precio del producto de la TBvalores de precios
+    // Puedes reemplazar este código con una llamada a una API o base de datos
+    const tablaPrecios = {
+        'Cazuela': 5000,
+        'Pollo con arroz': 4500,
+        'Garbanzos': 4500,
+        'Tequeños':3000
+    };
+
+    return tablaPrecios[producto];
+
+}
+
+function actualizarTotal() {
+    let sumaPrecios = 0;
+    
+    for (let i = 0; i < TBvalores.rows.length; i++) {
+      const celdaPrecio = TBvalores.rows[i].cells[3];
+      const precio = parseFloat(celdaPrecio.innerHTML);
+      sumaPrecios += precio;
+    }
+    
+    totalValores.innerHTML = sumaPrecios;
+  }
